431fd52b69239a8ce0841d0a10a77b0a8d4dd8d6
chongweiguan <75872136+chongweiguan@users.noreply.github.com>
2022-06-27 22:35:37 +0800

    Delete Secrets.py

--- CleanBot/Secrets.py
+++
@@ -1,6 +0,0 @@
-API_Token = '5316303881:AAEIysIUYoZ45d1EwN_5Jl6dGonJfv_ZE8g'
-Bot_URL = 'https://t.me/ezezezezezorderbot'
-Bot_Name = 'EzOrder'
-
-API_Token = '5374066926:AAE7IMAU8bjduSafS1DAzjk6Kpz6X9zIHQ0'
-Bot_URL = 'https://t.me/ezordertest_bot'
